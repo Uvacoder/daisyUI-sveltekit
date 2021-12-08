225a89fe9449c48cfa1b28d65fb61d29a7d095f9
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-12-08 01:26:03 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
 import preprocess from "svelte-preprocess";
+import vercel from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
+	    	adapter: vercel(),
 		target: '#svelte'
 	},
 
